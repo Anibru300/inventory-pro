413dfa6a0ec4fc6539895a426711678e99f55e6c
Anibru300 <ing.carlosurbina300@gmail.com>
2026-04-06 20:44:54 -0600

    Fix vite.config.js syntax error

--- inventory-pro-web/vite.config.js
+++ inventory-pro-web/vite.config.js
@@ -9,18 +9,8 @@ export default defineConfig({
       '@': resolve(__dirname, 'src'),
     },
   },
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
   build: {
     outDir: 'dist',
     sourcemap: true,
   },
-}
+})
